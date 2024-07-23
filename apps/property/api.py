3c4cfd98a39a668f0a7f2d1731095675331d4af4
administrador <administrador@ubuntusystem.myguest.virtualbox.org>
2024-07-22 20:29:39 -0400

    Actualizacion Clone Airbnb

--- apps/property/api.py
+++ apps/property/api.py
@@ -43,3 +43,31 @@ def properties_detail(request, pk):
     serializer = PropertiesDetailSerializer(property, many=False)
 
     return JsonResponse(serializer.data)
+
+
+@api_view(['POST'])
+def BookProperty(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
