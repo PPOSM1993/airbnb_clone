72ec97298dc93bec79a2b372394a8768b3d91a96
administrador <administrador@ubuntusystem.myguest.virtualbox.org>
2024-07-23 15:38:06 -0400

    Actualizacion  Clone Airbnb

--- clone_airbnb/app/landlords/[id]/page.tsx
+++ clone_airbnb/app/landlords/[id]/page.tsx
@@ -2,17 +2,28 @@ import React from 'react'
 import Image from 'next/image'
 import ContactButton from '@/app/components/ContactButton'
 import PropertyList from '@/app/components/properties/PropertyList'
+import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/action";
 
-const LandlordDetailPage = () => {
+const LandlordDetailPage = async ({ params }: { params: { id: string } }) => {
+    const landlord = await apiService.get(`/api/auth/${params.id}`)
+    const userId = await getUserId();
   return (
       <main className="max-w-[1500] mx-auto px-6 pb-6">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
             <aside className='"col-span-1 mb-4'>
-                <div className='flex flex-col items-center p-6 rounded-xl border border-gray-300 shadow-xl'>
-                    <Image src="/profile_pic_1.jpg" width={200} height={200} alt='Landlords name' className='rounded-full' />
-                    <h1 className='mt-6 2xl-1'>Landrod Name</h1>
+                <div className='flex flex-col items-center p-6 rousnded-xl border border-gray-300 shadow-xl'>
+                    <Image 
+                        src={landlord.avatar_url}
+                        width={200} height={200} 
+                        alt='Landlords name' 
+                        className='rounded-full' 
+                    />
+                      <h1 className='mt-6 2xl-1'>{landlord.name}</h1>
 
-                    <ContactButton/>
+                      {userId != params.id && (
+                          <ContactButton />
+                      )}
 
                 </div>
 
@@ -21,7 +32,7 @@ const LandlordDetailPage = () => {
 
             <div className="col-span-3 pl-0 md:pl-6">
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                    <PropertyList/>
+                      <PropertyList landlord_id={params.id} />
                 </div>
             </div>
         </div>
